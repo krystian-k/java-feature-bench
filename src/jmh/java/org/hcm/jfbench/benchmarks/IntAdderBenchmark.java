3df892d1e36ca2e6d2d2d06c0bfce5a6ee0a8a8f
Krystian Kowalski <krystian.kowalski@dynatrace.com>
2017-04-19 08:54:43 +0200

    benchmark using primitives added

--- src/jmh/java/org/hcm/jfbench/benchmarks/IntAdderBenchmark.java
+++ src/jmh/java/org/hcm/jfbench/benchmarks/IntAdderBenchmark.java
@@ -21,7 +21,15 @@ public class IntAdderBenchmark {
 		}
 	}
 
-	public static class Result<T extends Number> {
+	static class ResultLong {
+		long result;
+
+		ResultLong() {
+			result = 0L;
+		}
+	}
+
+	static class Result<T extends Number> {
 		T result;
 
 		Result(@NotNull T result) {
@@ -39,9 +47,35 @@ public class IntAdderBenchmark {
 	}
 
 	@Benchmark
+	public void testSimpleLoopResultLongInnerClass(Blackhole blackhole) {
+		ResultLong result = new ResultLong();
+		for (int v : IntegerList.integerList) {
+			result.result += v;
+		}
+		blackhole.consume(result.result);
+	}
+
+
+	@Benchmark
+	public void testSimpleLoopResultTemplateClass(Blackhole blackhole) {
+		final Result<Long> result = new Result<>(0L);
+		for (int v : IntegerList.integerList) {
+			result.result += v;
+		}
+		blackhole.consume(result.result);
+	}
+
+	@Benchmark
 	public void testStreamReduceMethod(Blackhole blackhole) {
 		long result;
 		result = IntegerList.integerList.stream().reduce(0, Integer::sum);
+		blackhole.consume(result);
+	}
+
+	@Benchmark
+	public void testMTStreamReduceMethod(Blackhole blackhole) {
+		long result;
+		result = IntegerList.integerList.parallelStream().reduce(0, Integer::sum);
 		blackhole.consume(result);
 	}
 
@@ -53,8 +87,15 @@ public class IntAdderBenchmark {
 	}
 
 	@Benchmark
-	public void testForEachStaticClass(Blackhole blackhole) {
+	public void testForEachResultTemplateClass(Blackhole blackhole) {
 		final Result<Long> result = new Result<>(0L);
+		IntegerList.integerList.forEach(v -> result.result += v);
+		blackhole.consume(result.result);
+	}
+
+	@Benchmark
+	public void testForEachResultLongClass(Blackhole blackhole) {
+		final ResultLong result = new ResultLong();
 		IntegerList.integerList.forEach(v -> result.result += v);
 		blackhole.consume(result.result);
 	}
